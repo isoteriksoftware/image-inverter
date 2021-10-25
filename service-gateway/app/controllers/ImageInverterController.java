929b611c61efd262d4db5f220fcac64437dffa9f
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-10-25 05:49:25 +0100

    fixed dependency errors

--- service-gateway/app/controllers/ImageInverterController.java
+++ service-gateway/app/controllers/ImageInverterController.java
@@ -3,6 +3,7 @@ package controllers;
 import actors.ImageInverterActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import io.swagger.annotations.*;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -14,6 +15,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 @Singleton
+@Api(value = "Image Inverter")
 public class ImageInverterController extends Controller {
     final ActorRef imageInverterActor;
 
@@ -24,16 +26,31 @@ public class ImageInverterController extends Controller {
         imageInverterActor = actorSystem.actorOf(ImageInverterActor.getProps());
     }
 
+    @ApiOperation(value = "Upload image to invert")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Image inverted")
+    })
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(
+                    name = "image",
+                    paramType = "form",
+                    required = true,
+                    type = "file",
+                    value = "The image file to invert"
+            )
+    })
     public CompletionStage<Result> invertImage() {
         Http.MultipartFormData<File> body = request().body().asMultipartFormData();
+        if (body == null)
+            return CompletableFuture.supplyAsync(() -> badRequest("No image file provided"));
+
         Http.MultipartFormData.FilePart<File> image = body.getFile("image");
 
         if (image == null)
-            return new CompletableFuture<>().thenApply(response ->
-                    badRequest("No image file provided"));
+            return CompletableFuture.supplyAsync(() -> badRequest("No image file provided"));
         else {
             File file = image.getFile();
-            return new CompletableFuture<>().thenApply(response ->
+            return CompletableFuture.supplyAsync(() ->
                     ok(file.getAbsolutePath()));
         }
     }
